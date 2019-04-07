e286a112e13e016f66616388f8961335ad496794
rounakdatta <rounakdatta12@gmail.com>
2019-04-07 16:55:55 +0530

    table pageSize update; readOnly clientxname bug fix

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ db = firebase.database()
 
 currentDomain = 'https://aops--spotifyrounak.repl.co'
 
-standardEmailHeader = 'ASSLPL Operations Notifications' + str(datetime.date.today().strftime('%d-%m-%Y'))
+standardEmailHeader = 'ASSLPL Operations Notifications for ' + str(datetime.date.today().strftime('%d-%m-%Y'))
 standardEmailContent = 'Summary of recent changes to ' + currentDomain + ':\n'
 
 code2task = {"delFromCFS": "Delivery From CFS", "delToClient": "Delivery to Client", "exportFromSource": "Export From SC", "exportFromTransit": "Export From TC", "importAtIndia": "", "importAtTransit": "Import At TC", "importEstimateAtIndia": "Import ETA At India", "importEstimateAtTransit": "Import ETA at TC", "invoiceDate": "Invoice Date", "invoiceNo": "Invoice Number", "sourceCountry": "Source Country", "transitCountry": "Transit Country", "bill": "Bill of Entry", "chaJobNo": "CHA Job No", "chaname": "CHA Name", "customDutyAmount": "Custom Duty Amount"}
